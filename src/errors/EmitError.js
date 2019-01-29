89559b57af5fb5105e2ef628e959f273b05b2304
Owen Bellish <shadowguy224@live.com>
2019-01-29 14:28:39 -0500

    require fix

--- src/errors/EmitError.js
+++ src/errors/EmitError.js
@@ -3,7 +3,10 @@ const FriendlyError = require('./FriendlyError');
  * An error for when emitting has an error
  * @extends {FriendlyError}
  */
-class EmitError extends require('./FriendlyError') {
-    constructor(message)
+class EmitError extends FriendlyError {
+    constructor(message) {
+        super(message);
+        this.name = 'EmitError';
+    }
 }
 module.exports = EmitError;
